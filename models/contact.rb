d9b269d349381fa834238ced81db94315e78d913
RockyLatchman <r.latchman3624@gmail.com>
2025-09-26 08:40:09 +0700

    Contact model

---
+++ models/contact.rb
@@ -0,0 +1,16 @@
+class Contact < ActiveRecord::Base
+   belongs_to :account
+
+   def retrieve_all()
+   end
+
+   def retrieve_contact()
+   end
+
+   def update_contact()
+   end
+
+   def remove_contact()
+   end
+
+end
