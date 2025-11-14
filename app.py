80e6f28838ec4166a204350f1006e1063eeb134a
RockyLatchman <r.latchman3624@gmail.com>
2025-11-14 14:29:33 -0800

    CSS to increase height and add right border to main sidebar

--- app.py
+++ app.py
@@ -1,72 +1,84 @@
-from flask import Flask, render_template, session, request
-from dotenv import load_dotenv
-from flask_wtf import CSRFProtect
-from sqlmodel import Session, create_engine
-from models import Account, Contact, Event, Message, Storage
-import os
-import datetime
 import calendar
+import datetime
+import os
 
+from dotenv import load_dotenv
+from flask import Flask, render_template, request, session
+from flask_wtf import CSRFProtect
+from models import Account, Contact, Event, Message, Storage
+from sqlmodel import Session, create_engine
 
-load_dotenv('.env')
+load_dotenv(".env")
 
 app = Flask(__name__)
-app.config['DATABASE_URL'] = os.environ.get('DATABASE_URL')
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-db_engine = create_engine(app.config['DATABASE_URL'], echo=True)
+app.config["DATABASE_URL"] = os.environ.get("DATABASE_URL")
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+db_engine = create_engine(app.config["DATABASE_URL"], echo=True)
 csrf_token = CSRFProtect(app)
 
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/register', methods=['GET', 'POST'])
+
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    return render_template('register.html')
+    return render_template("register.html")
 
-@app.route('/signout')
+
+@app.route("/signout")
 def signout():
-    return render_template('signout.html')
+    return render_template("signout.html")
 
-@app.route('/inbox')
+
+@app.route("/inbox")
 def inbox():
-    return render_template('inbox.html')
+    return render_template("inbox.html")
 
-@app.route('/inbox/send')
+
+@app.route("/inbox/send")
 def send_message():
     pass
 
-@app.route('/contacts')
-def contacts():
-    return render_template('contacts.html')
 
-@app.route('/calendar')
+@app.route("/contacts")
+def contacts():
+    return render_template("contacts.html")
+
+
+@app.route("/calendar")
 def pigeon_calendar():
     user_calendar = calendar.HTMLCalendar(firstweekday=0)
     current_date = datetime.datetime.today()
     return render_template(
-        'calendar.html',
-        current_day=current_date.strftime('%A'),
-        current_date=current_date.strftime('%d'),
-        cal=user_calendar.formatmonth(current_date.year, current_date.month)
+        "calendar.html",
+        current_day=current_date.strftime("%A"),
+        current_date=current_date.strftime("%d"),
+        cal=user_calendar.formatmonth(current_date.year, current_date.month),
     )
 
-@app.route('/calendar/event', methods=['POST'])
+
+@app.route("/calendar/event", methods=["POST"])
 def calendar_event():
-    #remove or add event to calendar
+    # remove or add event to calendar
     pass
 
-@app.route('/storage')
+
+@app.route("/storage")
 def storage():
-    return render_template('storage.html')
+    return render_template("storage.html")
 
-@app.route('/settings')
+
+@app.route("/settings")
 def settings():
-    return render_template('settings.html')
+    return render_template("settings.html")
 
-@app.route('/tags/<color>')
+
+@app.route("/tags/<color>")
 def tag(color):
     pass
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5555)
