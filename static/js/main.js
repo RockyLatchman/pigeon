b1c4dc800fcd98edc33743c42e29caa9c3ddf14e
RockyLatchman <r.latchman3624@gmail.com>
2025-11-18 10:58:36 -0800

    selectedMenuItem function for context menu

--- static/js/main.js
+++ static/js/main.js
@@ -1,23 +1,3 @@
-function toggleSearchField() {
-  const searchButton = document.querySelector(".search-button");
-  searchButton.addEventListener("click", (e) => {
-    const searchField = document.querySelector("#search");
-    const computedSearch = window.getComputedStyle(searchField);
-    if (computedSearch.display == "none") {
-      document.querySelector("#search").style.display = "block";
-      document.querySelector("#search").style.borderBottom = "1px solid #ccc";
-    } else {
-      document.querySelector("#search").style.display = "none";
-    }
-  });
-}
-
-function getCurrentDate() {
-  const date = new Date();
-  const day = date.getDate();
-  return day;
-}
-
 const calendarEvent = `
        <div class="calendar-event">
           <span><a href="">[X] CLOSE</a></span>
@@ -37,17 +17,38 @@ const calendarEvent = `
 const sidePanel = ``;
 const editContactForm = ``;
 const videoContentWindow = ``;
+const confirmationMenu = ``;
 const storageMenu = `
     <div class="document-menu">
        <menu>
-         <li>Open</li>
-         <li>Download</li>
-         <li>Rename</li>
-         <li>Get Info</li>
-         <li>Delete</li>
+         <li><a href="" id="open">Open</a></li>
+         <li><a href="" id="download">Download</a></li>
+         <li><a href="" id="rename">Rename</a></li>
+         <li><a href="" id="get-info">Get Info</a></li>
+         <li><a href="" id="delete">Delete</a></li>
        </menu>
     </div>
 `;
+
+function toggleSearchField() {
+  const searchButton = document.querySelector(".search-button");
+  searchButton.addEventListener("click", (e) => {
+    const searchField = document.querySelector("#search");
+    const computedSearch = window.getComputedStyle(searchField);
+    if (computedSearch.display == "none") {
+      document.querySelector("#search").style.display = "block";
+      document.querySelector("#search").style.borderBottom = "1px solid #ccc";
+    } else {
+      document.querySelector("#search").style.display = "none";
+    }
+  });
+}
+
+function getCurrentDate() {
+  const date = new Date();
+  const day = date.getDate();
+  return day;
+}
 
 function switchForm() {
   /* load Edit contact form  */
@@ -91,17 +92,68 @@ function addCalendarEvent() {
 }
 
 function openContextMenu() {
-  const documentItem = document.querySelectorAll(".content ul");
+  const documentItem = document.querySelectorAll(".kebab-menu");
   documentItem.forEach((item) => {
     item.addEventListener("click", (e) => {
       const menuHTML = loadTemplate(storageMenu);
       const menu = menuHTML.childNodes[1];
       menu.style.left = e.clientX + "px";
       menu.style.top = e.clientY + "px";
+      //item.parentNode.firstElementChild.innerHTML;
       document.querySelector("body").appendChild(menu);
+      selectedMenuItem(document.querySelector("#open"), item.parentNode);
+      selectedMenuItem(document.querySelector("#download"), item.parentNode);
+      selectedMenuItem(document.querySelector("#rename"), item.parentNode);
+      selectedMenuItem(document.querySelector("#get-info"), item.parentNode);
+      selectedMenuItem(document.querySelector("#delete"), item.parentNode);
     });
   });
 }
+
+function selectedMenuItem(menuItem, item) {
+  switch (menuItem.id) {
+    case "open":
+      menuItem.addEventListener("click", (e) => {
+        e.preventDefault();
+        openItem(item);
+      });
+      break;
+    case "download":
+      menuItem.addEventListener("click", (e) => {
+        e.preventDefault();
+        downloadItem(item);
+      });
+      break;
+    case "rename":
+      menuItem.addEventListener("click", (e) => {
+        e.preventDefault();
+        renameItem(item);
+      });
+      break;
+    case "get-info":
+      menuItem.addEventListener("click", (e) => {
+        e.preventDefault();
+        getItemInfo(item);
+      });
+      break;
+    case "delete":
+      menuItem.addEventListener("click", (e) => {
+        e.preventDefault();
+        deleteItem(item);
+      });
+      break;
+  }
+}
+
+function openItem(item) {}
+
+function downloadItem(item) {}
+
+function renameItem(item) {}
+
+function getItemInfo(item) {}
+
+function deleteItem(item) {}
 
 function highlightDate() {
   /*get the dates from the cells, iterate over them
